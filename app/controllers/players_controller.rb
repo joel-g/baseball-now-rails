2c9a8221ed6db5ec45ca060ffbc032aff5fdc6bd
Joel Guerra <joelaguerra1@gmail.com>
2017-08-08 11:01:49 -0700

    working player create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,4 +9,17 @@ class PlayersController < ApplicationController
     render :new
   end
 
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def player_params
+    params.require(:player).permit(:name, :location, :pitches_faced, :hits, :innings, :date)
+  end
+
 end
