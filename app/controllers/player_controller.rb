7edca29bb38b637b94325c2f3e20cb3058204706
Joel Guerra <joelaguerra1@gmail.com>
2017-08-08 10:11:23 -0700

    adds routes for Player, adds player_controller, adds view files

---
+++ app/controllers/player_controller.rb
@@ -0,0 +1,7 @@
+class PlayersController < ApplicationController
+
+  def index
+    render :index
+  end
+
+end
--- db/migrate/20170808165102_create_players.rb
+++ db/migrate/20170808165102_create_players.rb
@@ -1,7 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration[5.1]
   def change
     create_table :players do |t|
-      t.string :name,
+      t.string :name
       t.string :location
       t.integer :pitches_faced
       t.integer :hits
