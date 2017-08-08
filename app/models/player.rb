51d9c380697b1d4337134f9f50c5432afbec5843
Joel Guerra <joelaguerra1@gmail.com>
2017-08-08 14:07:18 -0700

    validates from zero and negative numbers

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,6 @@
 class Player < ApplicationRecord
   validates_presence_of :name, :pitches_faced, :hits, :date
+  validates :pitches_faced, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+  validates :hits, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :innings, :numericality => { :greater_than_or_equal_to => 1 }
 end
