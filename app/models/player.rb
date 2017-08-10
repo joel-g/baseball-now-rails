6a570dc5d2a081a6ec5803d71567544462a8d548
Joel Guerra <joelaguerra1@gmail.com>
2017-08-10 12:51:04 -0700

    fixed model validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  validates_presence_of :name, :pitches_faced, :hits, :date
+  validates_presence_of :name, :date
   validates :pitches_faced, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 50 }
   validates :hits, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :innings, numericality: { greater_than_or_equal_to: 1 }
