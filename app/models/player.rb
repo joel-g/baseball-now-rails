695b31b590c05a8195297a2dcf6f0e97f3ef4489
Joel Guerra <joelaguerra1@gmail.com>
2017-08-11 15:42:39 -0700

    validates max innings count

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   validates_presence_of :name, :date
   validates :pitches_faced, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 50 }
   validates :hits, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-  validates :innings, numericality: { greater_than_or_equal_to: 1 }
+  validates :innings, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 20 }
 end
